a95f728e172d1e3d7eb9617783104dcc087015e3
mellison <mellisonevolve@gmail.com>
2020-02-09 23:32:52 -0500

    Added Python3x capability to return

--- time_class.py
+++ time_class.py
@@ -12,30 +12,29 @@ class Time:
         print('%.2d:%.2d:%.2d' % (self.hour, self.minute, self.second))
 
     def init_to_time(seconds):
-
-    time = Time()
-    minutes, time.second = divmod(seconds, 60)
-    time.hour, time.minute = divmod(minutes, 60)
-    return time 
+        time = Time()
+        minutes, time.second = divmod(seconds, 60)
+        time.hour, time.minute = divmod(minutes, 60)
+        return(time) 
 
     def time_to_int(self):
         minutes = time.hour * 60 + self.minute 
         seconds = minutes * 60 + self.second 
-        return seconds
+        return(seconds)
 
     def __add__(self, other):
         if isinstance(other, Time):
             return self.add_time(other)
         else:
-            return self.increment(other)
+            return(self.increment(other))
 
     def add_time(self, other):
         seconds = self.time_to_init() + other.time_to_int()
-        return int_to_time(seconds)
+        return(int_to_time(seconds))
     
     def increment(self, seconds):
         seconds += self.time_to_int()
-        return int_to_time(seconds) 
+        return(int_to_time(seconds))
 
 
 
