5c24ed5c932f05845a49f3f410d2fd65c5c787f4
mellison <mellisonevolve@gmail.com>
2020-04-29 21:04:44 -0400

    Recursion using ruby sample

--- Recursion.rb
+++ Recursion.rb
@@ -1,18 +1,17 @@
-# Sample 1 
+# Sample 1: iterative vs recursie function 
 
-# Iteractive factorial 
+# This is an Iterative factorial 
 def iterative_factorial n
     # Factorial of 5 = 1 * 2 *3 * 4 * 5 
     (1..n).inject(:*)
 end 
 
-# Recrusive factorial
+# This is an Recrusive factorial
 def recursive_factorial n
     return 1 if n <= 1 
 
     # recursive call 
     n * recursive_factorial(n-1)
 end 
-
   
 recursive_factorial(5)
