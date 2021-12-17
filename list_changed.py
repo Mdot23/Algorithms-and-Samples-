c8e0f6bf4128381e6e1162f5155a328363756098
Milton <ellimilt@amazon.com>
2021-12-16 18:19:07 -0800

    Added another sample algorithim

--- list_changed.py
+++ list_changed.py
@@ -6,3 +6,12 @@ def get_new_elem(old_list, new_list):
             new_elems.append(elem)
     return new_elems
 
+# Determine if the dynamo_vips data has changed since the new or current.
+def get_dns_data(dynamo_vips, drrd):
+    new_elems = []
+    for elem in drrd:
+        if elem not in dynamo_vips:
+            new_elems.append(elem)
+    return new_elems
+
+
